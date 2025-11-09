bbdc611e9d788e4bb8554b30229e34c77800a14f
Leo Borai <estebanborai@gmail.com>
2025-11-09 09:41:55 +0100

    feat: serve ui client assets

---
+++ src/server/src/server/router/assets.rs
@@ -0,0 +1,49 @@
+use axum::response::{IntoResponse, Response};
+use http::{Uri, header};
+use rust_embed::Embed;
+
+#[derive(Embed)]
+#[folder = "../client/dist"]
+struct Asset;
+
+pub struct StaticFile<T>(pub T);
+
+impl<T> IntoResponse for StaticFile<T>
+where
+    T: Into<String>,
+{
+    fn into_response(self) -> Response {
+        let path = self.0.into();
+
+        match Asset::get(path.as_str()) {
+            Some(content) => {
+                let mime = mime_guess::from_path(path).first_or_octet_stream();
+                (
+                    [
+                        (header::CONTENT_TYPE, mime.as_ref()),
+                        (header::CACHE_CONTROL, "max-age=604800"),
+                    ],
+                    content.data,
+                )
+                    .into_response()
+            }
+            None => {
+                let bytes = Asset::get("index.html")
+                    .expect("index.html should exist")
+                    .data;
+
+                ([(header::CONTENT_TYPE, "text/html")], bytes).into_response()
+            }
+        }
+    }
+}
+
+pub async fn serve_asset(uri: Uri) -> impl IntoResponse {
+    let mut path = uri.path().trim_start_matches('/').to_string();
+
+    if path.starts_with("dist/") {
+        path = path.replace("dist/", "");
+    }
+
+    StaticFile(path)
+}
--- src/server/src/server/router/mod.rs
+++ src/server/src/server/router/mod.rs
@@ -1,15 +1,20 @@
 pub mod api;
+pub mod assets;
 
 use anyhow::Result;
+use axum::routing::get;
 use axum::{Extension, Router};
 use tower_http::trace::{DefaultMakeSpan, DefaultOnResponse, TraceLayer};
 use tracing::Level;
 
 use crate::services::SharedServices;
 
+use self::assets::serve_asset;
+
 pub async fn make_router(services: SharedServices) -> Result<Router> {
     let router = axum::Router::new()
         .nest("/api", api::make_api_router())
+        .fallback_service(get(serve_asset))
         .layer(
             TraceLayer::new_for_http()
                 .make_span_with(DefaultMakeSpan::new().level(Level::INFO))
